0a0db621d6fc6bd4b2764bf3e4d8f9416aca0d2f
ccalantoc <candicecalantoc@gmail.com>
2017-07-05 14:13:06 -0700

    message

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'epd8_dz7673%u3y(5pi+o8_87z9oi3^j=wa29(8mzxg5i_j%g4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','ccalantoc.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
